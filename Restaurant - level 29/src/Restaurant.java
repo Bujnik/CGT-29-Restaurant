92e1a88e0704753107e950068ffdab72a7df1369
Dawid Bujnicki <89477976+Bujnik@users.noreply.github.com>
2021-11-20 21:12:46 +0100

    Part1
    Created stubs of files used for order processing - Restaurant has main() method, simulating real thing

---
+++ Restaurant - level 29/src/Restaurant.java
@@ -0,0 +1,10 @@
+package com.codegym.task.task27.task2712;
+
+
+
+public class Restaurant {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ Restaurant - level 29/src/kitchen/Dish.java
@@ -0,0 +1,16 @@
+package com.codegym.task.task27.task2712.kitchen;
+
+public enum Dish {
+    Fish,
+    Steak,
+    Soup,
+    Juice,
+    Water;
+
+    public static String allDishesToString(){
+        StringBuilder sb = new StringBuilder();
+        for (Dish dish : Dish.values()) sb.append(dish).append(", ");
+        sb.replace(sb.lastIndexOf(","),sb.length()+1,"");
+        return sb.toString();
+    }
+}
