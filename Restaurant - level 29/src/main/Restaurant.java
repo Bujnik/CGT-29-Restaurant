5695aa45601ab46084171573f23609d145f5c494
Dawid Bujnicki <89477976+Bujnik@users.noreply.github.com>
2021-11-25 23:14:15 +0100

    Part 4
    Now we added Waiter to our restaurant. In this case - Waiter is our Observer and Cook is one to be observed (Observable)

--- Restaurant - level 29/src/main/Restaurant.java
+++ Restaurant - level 29/src/main/Restaurant.java
@@ -2,11 +2,14 @@ package main;
 
 
 import main.kitchen.Cook;
+import main.kitchen.Waiter;
 
 public class Restaurant {
     public static void main(String[] args) {
         Tablet tablet = new Tablet(76);
         Cook amigoCook = new Cook("Amigo");
+        Waiter waiter = new Waiter();
+        amigoCook.addObserver(waiter);
         tablet.addObserver(amigoCook);
         tablet.createOrder();
         tablet.createOrder();
